ba142642735e5bff35bb8ac5bedcda55c423e77e
kans <kans@wxh.local>
2016-03-30 09:31:39 +0800

    Initial Commit

---
+++ collectionHearder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  collectionHearder
+//
+//  Created by kans on 16/3/30.
+//  Copyright © 2016年 HaiYn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ collectionHearder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  collectionHearder
+//
+//  Created by kans on 16/3/30.
+//  Copyright © 2016年 HaiYn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
